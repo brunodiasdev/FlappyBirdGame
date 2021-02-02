31749a129ce394b6bac1e70a6deed9a7c8e56590
brunodiasdev <brunodiasdevs@gmail.com>
2021-02-02 07:26:55 -0300

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -1,9 +1,10 @@
+//Função utilizada para criação dos elementos DOM e das classes.
 function novoElemento(tagName, className){
     const elem = document.createElement(tagName)
     elem.className = className
     return elem
 }
-
+//Função para criar as barreiras em alternancia.
 function Barreira(reversa = false){
     this.elemento = novoElemento('div', 'barreira') 
     const borda = novoElemento('div', 'borda')
