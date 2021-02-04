2e679ed93eef47c298b0b0435b3916968e56f62a
brunodiasdev <brunodiasdevs@gmail.com>
2021-02-03 21:42:04 -0300

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -73,7 +73,20 @@ function Passaro(alturaJogo){
 
     this.elemento = novoElemento('img', 'passaro')
     this.elemento.src = 'imgs/passaro.png'
+    
     this.getY = () => parseInt(this.elemento.style.bottom.split('px')[0])
+    this.setY = () => this.elemento.style.bottom = `${y} px`
+
+    window.onkeydown = e => voando = true
+    window.onkeyup = e => voando = false
+
+    this.animar = () => {
+        const novoY = this.getY() + (voando ? 8 : -5)
+        const alturaMaxima = alturaJogo - this.elemento.clientHeight
+        if (novoY <= 0) {
+            this.setY(0)
+        }
+    }
 }
 
 const barreiras = new Barreiras(600, 1100, 200, 400)
