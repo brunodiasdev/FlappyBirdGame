f72e68d0409e063d7829b0758dfd7bcacb4bdb23
brunodiasdev <brunodiasdevs@gmail.com>
2021-02-06 18:32:28 -0300

    Update flappy.js

--- js/flappy.js
+++ js/flappy.js
@@ -121,6 +121,18 @@ function estaoSobrepostos(elementoA, elementoN){
 
     const horizontal = a.left + a.width >= b.left && b.left + b.width >= a.left
     const vertical = a.top + a.height >= b.top && b.top +b.height >= a.top
+
+    return horizontal && vertical
+}
+
+function colidiu (passaro, barreiras){
+    let colidiu = falsebarreiras.pares.forEach(parDeBarreiras => {
+        if (!colidiu){
+            const superior = parDeBarreiras.superior.elemento
+            const inferior = parDeBarreiras.inferior.elemento
+            colidiu = estaoSobrepostos(passaro.elemento, superior) || estaoSobrepostos(passaro.elemento, inferior)
+        }
+    })
 }
 
 function FlappyBird(){
@@ -142,6 +154,10 @@ function FlappyBird(){
         const temporizador = setInterval(() =>{
             barreiras.animar()
             passaro.animar()
+
+            if (colidiu(passaro, barreiras)){
+                clearInterval(temporizador)
+            }
         }, 20)
     }
 }
